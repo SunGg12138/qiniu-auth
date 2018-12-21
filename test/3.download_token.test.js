c94c75b24751a4b8da432e3b70cb52015c00a5ad
SunGg12138 <961154209@qq.com>
2018-12-21 18:23:24 +0800

    下载凭证及对应的测试用例

---
+++ test/3.download_token.test.js
@@ -0,0 +1,45 @@
+try {
+  require('./resource/qiniu.config');
+} catch (error) {
+  throw new Error(`
+  先配置你的/test/resource/qiniu.config.json文件再测试
+  qiniu.config.json是放置AccessKey和SecretKey的配置文件
+  1. 配置你的AccessKey和SecretKey到/test/resource/qiniu.config.default.json 
+  2. qiniu.config.default.json 改名为qiniu.config.json
+  `);
+}
+
+const fs = require('fs');
+const expect = require('chai').expect;
+const rp = require('node-request-slim').promise;
+const qiniu_config = require('./resource/qiniu.config');
+const common = require('./common');
+const qiniu_auth = require('../lib');
+
+describe('3. download_token（下载凭证）相关测试', function(){
+  this.timeout(20000);
+  before(async () => {
+    bucketName = Date.now() + '';
+    await common.createBucket(bucketName, qiniu_config);
+    await common.uploadFile(__filename, bucketName, 'test.js', qiniu_config);
+    await common.private(bucketName, qiniu_config);
+    domain = await common.domain(bucketName, qiniu_config);
+  });
+  it('使用下载凭证下载私有文件', async function(){
+    let url = 'http://' + domain + '/test.js';
+    let writeStrem = fs.createWriteStream(__dirname + '/resource/download.test.js');
+    // 不使用下载凭证下载时
+    let result_1 = await rp({ url });
+    expect(result_1.error === 'download token not specified').to.be.ok;
+
+    // 使用下载凭证下载时
+    let realUrl = qiniu_auth.download_token.call(qiniu_config, { url });
+    await rp({ url: realUrl, pipe: writeStrem });
+    let before = fs.readFileSync(__filename).toString();
+    let after = fs.readFileSync(__dirname + '/resource/download.test.js').toString();
+    expect(before === after).to.be.ok;
+  });
+  after(async () => {
+    await common.delBucket(bucketName, qiniu_config);
+  });
+});
--- test/common/index.js
+++ test/common/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const qiniu_auth = require('../../lib/');
 const rp = require('node-request-slim').promise;
 
@@ -30,4 +31,60 @@ async function delBucket(name, ak_sk){
     }
   });
   if (result.error) return Promise.reject(result);
+}
+
+exports.uploadFile = 
+async function uploadFile(path, bucketName, fileName, ak_sk, private){
+  let auth = qiniu_auth.upload_token.call(ak_sk, { scope: bucketName + ':' + fileName, returnBody: '{"name": $(fname) }' });
+  let request_options = {
+    method: 'POST',
+    url: 'http://up-z0.qiniup.com',
+    formData: {
+      scope: bucketName + ':' + fileName, 
+      key: fileName, fileName: fileName,
+      token: auth, file: fs.createReadStream(path)
+    }
+  };
+  let result = await rp(request_options);
+  if (result.error) return Promise.reject(result);
+}
+
+exports.private =
+async function private(bucketName, ak_sk){
+  let form = {
+    bucket: bucketName,
+    private: 1
+  };
+  // 获取授权
+  let auth = qiniu_auth.access_token.call(ak_sk, { path: '/private', form });
+  // 设置 Bucket 访问权限
+  let result = await rp({
+    url: 'http://uc.qbox.me/private',
+    method: 'POST',
+    form: form,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': auth
+    }
+  });
+  if (result.error) return Promise.reject(result);
+}
+
+exports.domain =
+async function domain(bucketName, ak_sk){
+  // 设置path和query
+  let path = '/v6/domain/list';
+  let query = 'tbl=' + bucketName;
+  // 获取授权
+  let auth = qiniu_auth.access_token.call(ak_sk, { path, query });
+  // 获取储存空间域名
+  let result = await rp({
+    url: 'http://api.qiniu.com' + path + '?' + query,
+    method: 'GET',
+    headers: {
+      'Authorization': auth
+    }
+  });
+  if (result.error) return Promise.reject(result);
+  return result[0];
 }
